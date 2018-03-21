446358af33c0811339ad0dbd05c55482333c76f1
Lin  Peng <lin.peng@anu.edu.au>
2018-03-21 20:34:44 +1100

    bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {CalendarModule} from "ap-angular2-fullcalendar";
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+		BrowserModule,
+		CalendarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
