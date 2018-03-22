10d9dac29f266004be1e6dab32de959e220b02b8
Lin  Peng <lin.peng@anu.edu.au>
2018-03-22 18:57:29 +1100

    linked new component to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {CalendarModule} from "ap-angular2-fullcalendar";
-
+import {MatCardModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
+import {MatDialogModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { DialogDemoComponent } from './dialog-demo/dialog-demo.component';
+
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-		BrowserModule,
-		CalendarModule
-  ],
+  declarations: [AppComponent, DialogDemoComponent],
+  imports: [BrowserModule,CalendarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/dialog-demo/dialog-demo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+@Component({
+  selector: 'app-dialog-demo',
+  templateUrl: './dialog-demo.component.html',
+  styleUrls: ['./dialog-demo.component.css']
+})
+export class DialogDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
