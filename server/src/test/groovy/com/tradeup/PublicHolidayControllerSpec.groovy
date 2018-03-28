dab1fd48ffca99429fd57f108e53782b6e62a598
HongguLin <honggulin@gmail.com>
2018-03-28 14:28:17 +1100

    initialize database and backend setup

---
+++ server/src/test/groovy/com/tradeup/PublicHolidayControllerSpec.groovy
@@ -0,0 +1,18 @@
+package com.tradeup
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class PublicHolidayControllerSpec extends Specification implements ControllerUnitTest<PublicHolidayController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+        true == false
+    }
+}
